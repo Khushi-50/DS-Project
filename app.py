887b67a73cec321ad37c456cef9b3235f0275842
Khushi-50 <kaggarwal3031@gmail.com>
2025-05-06 02:33:50 +0530

    More features

--- app.py
+++ app.py
@@ -1,27 +1,49 @@
 import streamlit as st
 import dss as dss
 import matplotlib.pyplot as plt 
+import yfinance as yf
+import pandas as pd
+import altair as alt
+from vega_datasets import data
 
 st.title('Stock Analysis Dashboard')
+company_to_ticker = {
+    "Apple": "AAPL",
+    "Google": "GOOGL",
+    "Microsoft": "MSFT",
+    "Tesla": "TSLA",
+    "Amazon": "AMZN",
+    "Reliance": "RELIANCE.NS",
+    "TCS": "TCS.NS",
+    "Infosys": "INFY.NS",
+    "Netflix":"NFLX"
+}
+Company=st.selectbox("Name of Company:",['Apple','Google','Microsoft','Amazon','Tesla','Reliance','TCS','Infosys','Netflix'])
+if Company in company_to_ticker :
+    ticker=company_to_ticker[Company]
+elif Company not in company_to_ticker:
+    st.error('Enter a valid Company')
+stock_data=yf.download({ticker},start='2023-01-01',end='2025-01-01')
+data=pd.DataFrame(stock_data)
 
 st.success('HeHe You Opened My dashboard .. STUPID PEEPAL')
 st.subheader('Data Description')
-st.dataframe(dss.basic_stats())
+st.dataframe(dss.basic_stats(data))
 st.subheader('Stock Close Price')
-fig=dss.plot_data()
+fig=dss.plot_data(ticker,data)
 st.pyplot(fig)
 
 st.subheader('Moving Averages')
 #st.slider('Choose Time Frame for Moving Average',0,100,50,step=10)
-fig1=dss.plot_mov_avg()
+fig1=dss.plot_mov_avg(ticker,data)
 st.pyplot(fig1)
 
 st.subheader('Volatility')
-st.write(dss.Volatility())
+st.write(dss.Volatility(ticker,data))
 
 st.subheader('Cumulative Returns')
-fig2=dss.cumulativeReturn()
+fig2=dss.cumulativeReturn(ticker,data)
 st.pyplot(fig2)
 
 st.subheader('Sharpe Ratio')
-st.write(dss.sharpe_Ratio())
+st.write(dss.sharpe_Ratio(ticker,data))
--- dss.py
+++ dss.py
@@ -4,21 +4,7 @@ import matplotlib as my
 import yfinance as yf
 import matplotlib.pyplot as plt
 
-company_to_ticker = {
-    "Apple": "AAPL",
-    "Google": "GOOGL",
-    "Microsoft": "MSFT",
-    "Tesla": "TSLA",
-    "Amazon": "AMZN",
-    "Reliance": "RELIANCE.NS",
-    "TCS": "TCS.NS",
-    "Infosys": "INFY.NS"
-}
-stock_data=yf.download('NFLX',start='2023-01-01',end='2025-01-01')
-#print(stock_data.head())
-data=pd.DataFrame(stock_data)
-
-def cleaning_data():
+def cleaning_data(ticker,data):
     #print(data)
     data.dropna(inplace=True)
     #print(new_data.to_string())
@@ -26,38 +12,38 @@ def cleaning_data():
     #print(new_data.duplicated())
     data.drop_duplicates(inplace=True)
 
-def basic_stats():
+def basic_stats(data):
     return data.describe() 
 
  
-def plot_data():
+def plot_data(ticker,data):
     plt.figure(figsize=(9, 6))
     plt.plot(data['Close'], label='Closing Price')
-    plt.title('Netflix Stock Price (2023-2025)')
+    plt.title(f'{ticker} Stock Price (2023-2025)')
     plt.xlabel('Date')
     plt.ylabel('Stock Price (USD)')
     plt.legend(shadow=True)
     plt.grid(True)
 
-def plot_mov_avg():
+def plot_mov_avg(ticker,data):
     data['50_day_ma'] = data['Close'].rolling(window=50).mean()
     data['100_day_ma'] = data['Close'].rolling(window=100).mean()
     plt.figure(figsize=(10,6))
     plt.plot(data['Close'], label='Closing Price')
     plt.plot(data['50_day_ma'],label='50 Day moving average')
     plt.plot(data['100_day_ma'],label='100 Day moving average')
-    plt.title('Netflix Stock moving Average')
+    plt.title(f'{ticker} Stock moving Average')
     plt.xlabel('Date')
     plt.ylabel('Stock price(USD)')
     plt.legend()
     plt.grid(True)
 
-def Volatility():
+def Volatility(ticker,data):
     data['Daily Return']=data['Close'].pct_change()
     volatility=data['Daily Return'].std()
     return (f"Annualized Volatility: {volatility:.2%}")
 
-def cumulativeReturn():
+def cumulativeReturn(ticker,data):
     data['Daily return']=data['Close'].pct_change()
     data['Cumulative Return'] = (1 + data['Daily Return']).cumprod()
     plt.figure(figsize=(10, 6))
@@ -69,7 +55,7 @@ def cumulativeReturn():
     plt.grid(True)
 
  
-def sharpe_Ratio():
+def sharpe_Ratio(ticker,data):
     risk_free_rate = 0
     data['Daily return']=data['Close'].pct_change()
     excess_daily_return = data['Daily Return'] - risk_free_rate
